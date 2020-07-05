0fdaafd0a28203b5f83a9a2e748a49d554b8ddfa
allen900625@gmail.com <allen890612>
2020-07-05 11:12:22 +0800

    fix issues about News management

--- news-web/src/main/java/com/allenliu/news/web/NewsInfoController.java
+++ news-web/src/main/java/com/allenliu/news/web/NewsInfoController.java
@@ -8,16 +8,14 @@ import com.allenliu.news.framework.ApiResult;
 import com.allenliu.news.service.busi.impl.NewsInfoServiceImpl;
 import com.allenliu.news.service.es.ElasticSearchResult;
 import com.allenliu.news.service.es.impl.ElasticSearchServiceImpl;
-import com.github.pagehelper.PageInfo;
 import io.searchbox.core.SearchResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -40,18 +38,39 @@ public class NewsInfoController {
   @PostMapping("add")
   @ApiOperation("发布新闻")
   public ApiResult createNews(@RequestBody NewsInfo newsInfo){
-    newsInfoService.save(newsInfo);
-    JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(newsInfo));
-    elasticSearchService.createIndex(jsonObject,"newsinfo","newsinfo",newsInfo.getId().toString());
+    int count = newsInfoService.save(newsInfo);
+    //成功新增新闻，刷新es
+    if(count > 0){
+      JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(newsInfo));
+      elasticSearchService.createIndex(jsonObject,"newsinfo","newsinfo",newsInfo.getId().toString());
+    }
+    return ApiResult.buildSuccess();
+  }
+
+  @PostMapping("edit")
+  @ApiOperation("编辑新闻")
+  public ApiResult updateNews(@RequestBody NewsInfo newsInfo){
+    int count = newsInfoService.update(newsInfo);
+    //成功更新且更新不是删除新闻，刷新es
+    if(count > 0 && newsInfo.getDeleted() == 0){
+      JSONObject jsonObject = JSONObject.parseObject(JSON.toJSONString(newsInfo));
+      elasticSearchService.updateDocument(jsonObject,"newsinfo","newsinfo",newsInfo.getId().toString());
+    }
     return ApiResult.buildSuccess();
   }
 
   @PostMapping("list")
   @ApiOperation("从es中查询新闻列表")
-  public ApiResult list(@RequestBody(required = false) NewsInfoPage newsInfo){
+  public ApiResult list(@RequestBody NewsInfoPage newsInfo){
     Map<String,String> map = new HashMap<>(2);
-    map.put("newsTitle",newsInfo.getNewsTitle());
-    map.put("newsContent",newsInfo.getNewsContent());
+    String newsTitle = newsInfo.getNewsTitle();
+    if(StringUtils.isNotEmpty(newsTitle)){
+      map.put("newsTitle",newsTitle);
+    }
+    String newsContent = newsInfo.getNewsContent();
+    if(StringUtils.isNotEmpty(newsContent)){
+      map.put("newsContent",newsContent);
+    }
     ElasticSearchResult result = elasticSearchService.createSearch(map, "newsinfo", new NewsInfo(), newsInfo.getPageSize(), newsInfo.getPageSize()*(newsInfo.getPageNum()-1), "newsTitle","newsContent");
     List<SearchResult.Hit<NewsInfo, Void>> list = result.getData();
     List<NewsInfo> newsInfos = new ArrayList<>();
